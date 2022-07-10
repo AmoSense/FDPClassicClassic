91e27c70c64eb49bd09b0ef1715d57cdebfbc27a
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-07-10 23:33:30 +0100

    oh yeah fdp uses translation files :/

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/LegacyCharms.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/LegacyCharms.kt
@@ -10,7 +10,7 @@ import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.value.BoolValue
 
-@ModuleInfo(name = "LegacyChams", description = "Allows you to see targets through blocks.", category = ModuleCategory.RENDER)
+@ModuleInfo(name = "LegacyChams", category = ModuleCategory.RENDER)
 class LegacyChams : Module() {
     val targetsValue = BoolValue("Targets", true)
     val chestsValue = BoolValue("Chests", true)
